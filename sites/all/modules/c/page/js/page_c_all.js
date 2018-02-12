10244acee35a3f79304a3facba21e9435b5a3a04
Double H <h@obserbot.com>
2018-02-13 06:41:30 +0800

    page module

---
+++ sites/all/modules/c/page/js/page_c_all.js
@@ -0,0 +1,79 @@
+(function($) {
+
+  var more_status = 'idle';
+  var last_created = 0;
+
+  function show_more_items(newsArr) {
+    var $wrapper = $('.main-content-wrapper');
+    for (var ix in newsArr) {
+      var odd = ix % 2 === 0 ? " news-odd-row" : "";
+      var itemHTML = '<div class="news-item-wrap' + odd + '">' +
+                       '<div class="news-title">' +
+                         '<a href="' + newsArr[ix]['url_origin'] + '">' + newsArr[ix]['title'] + '</a>' +
+                       '</div>' +
+                       '<div class="news-footer">' + newsArr[ix]['created'] + '</div>' +
+                      '</div>';
+      $wrapper.append(itemHTML);
+
+    }
+
+    if (newsArr.length < 16) {
+      $('#item-more9').hide();
+    }
+  }
+
+
+  Drupal.behaviors.muread_home9 = {
+    attach: function(context, settings) {
+
+      var last_reddit_id = settings.home9Args.last_reddit_id;
+      var lang_prefix = settings.home9Args.lang_prefix;
+      last_created = settings.home9Args.last_created;
+
+      // Click More
+      $('#item-more9').on('click', function(e) {
+
+        if ($('#more-spinner').hasClass('hidden') && last_created > 10000) {
+          $.post('more9', {last_created:last_created, lang_prefix:lang_prefix}, function(result) {
+
+            console.log(result);
+            show_more_items(result.news_array);
+
+            $('#more-spinner').addClass('hidden');
+            more_status = 'idle';
+
+            last_created = result.last_created;
+
+            /*
+
+            if (result.length < 8) {
+                $('.more-bar').parent().addClass('hidden');
+            }
+            */
+          }, 'json');
+
+          $('#more-spinner').removeClass('hidden');
+          more_status = 'sending';
+        }
+      });
+
+
+      /*
+      var html = '';
+      var newsItems = Drupal.settings.muread_home.news_items;
+
+      var odd = false;
+      for (var ix in newsItems) {
+        html += '<div class="news-item-wrap' + (odd ? " news-odd-row" : "") + '"><a href="' +
+          newsItems[ix].url_origin + '" target="_blank">' + newsItems[ix].title + '</a></div>';
+        odd = !odd;
+      }
+
+      $('.main-content-wrapper').append(html);
+      */
+
+
+    }
+  };
+
+})(jQuery);
---
+++ sites/all/modules/c/page/templates/c_all.tpl.php
@@ -0,0 +1,30 @@
+<div class="row home-content">
+  <div class="col-xs-12 col-sm-8">
+    <div class="main-content-wrapper">
+    <?php foreach ($news_items as $key => $item): ?>
+      <div class="news-item-wrap<?php print $key % 2 === 0 ? " news-odd-row" : ""; ?>">
+        <div class="news-title">
+          <a href="<?php print $item['url_origin']; ?>"><?php print $item['title']; ?></a>
+        </div>
+        <div class="news-footer">
+          <?php print $item['created']; ?>
+        </div>
+      </div>
+    <?php endforeach; ?>
+    </div>
+
+    <div id="item-more9">
+      <div class="btn-lg text-center">
+        <span class="spinner-wrap">
+          <span class="spinner hidden" id="more-spinner">
+            <img src="../sites/all/modules/c/home/images/spinner.gif" alt="">
+          </span>
+          <span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span>
+          <span class="more-text"> <?php print t('More'); ?></span>
+        </span>
+      </div>
+    </div>
+  </div>
+</div>
+
+
