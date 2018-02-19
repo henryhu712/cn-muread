e6d3d601d417ce130657ca29e49d6a0d4d98c55d
Double H <h@obserbot.com>
2018-02-19 20:44:13 +0800

    dd

--- sites/all/modules/c/home/templates/home.tpl.php
+++ sites/all/modules/c/home/templates/home.tpl.php
@@ -1,8 +1,13 @@
 
-        <div class="row items-container text-center">
+      <div class="row">
+        <?php print $webform_paste_url; ?>
+      </div>
+      <div class="row">
+        <div class="items-container text-center">
           <a href="/c/all">
             推荐文章数量： <?php print $number_ding_yue_hao; ?>
           </a>
         </div>
+      </div>
 
 
