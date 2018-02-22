d128dc987a26ff2cbc5cf79094c7281356b5f922
Double H <h@obserbot.com>
2018-02-22 21:52:33 +0800

    dd

--- sites/all/modules/c/home/templates/home.tpl.php
+++ sites/all/modules/c/home/templates/home.tpl.php
@@ -1,10 +1,17 @@
-
-      <div class="row">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-8">
         <div class="items-container text-center">
           <a href="/c/all">
             推荐文章数量： <?php print $number_ding_yue_hao; ?>
           </a>
         </div>
       </div>
+      <div class="col-sm-4">
+关注公众号：必读推荐
+
+      </div>
+    </div>
+  </div>
 
 
