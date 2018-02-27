e9f6092675251fba32a65488c8c9885f2e9e27c5
Double H <h@obserbot.com>
2018-02-27 13:10:43 +0800

    dd

--- sites/all/modules/c/home/templates/home.tpl.php
+++ sites/all/modules/c/home/templates/home.tpl.php
@@ -1,9 +1,24 @@
   <div class="container">
     <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+        <div class="row">
+          <div class="col-xs-4">
+            <?php foreach ($terms as $term): ?>
+            <div class="term-wrap">
+              <a href="/c/<?php print $term['alias']; ?>">
+                <?php print $term['name']; ?>
+              </a>
+            </div>
+            <?php endforeach; ?>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
       <div class="col-sm-8">
         <div class="items-container text-center">
           <a href="/c/all">
-            推荐文章数量： <?php print $number_ding_yue_hao; ?>
+            全部推荐文章： <?php print $number_ding_yue_hao; ?>
           </a>
         </div>
       </div>
