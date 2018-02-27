5fee7be9fafc0df4888ef24153be14b48a4d79e3
Double H <h@obserbot.com>
2018-02-27 18:10:17 +0800

    dd

--- sites/all/themes/ob/templates/page.tpl.php
+++ sites/all/themes/ob/templates/page.tpl.php
@@ -79,6 +79,9 @@
       <?php if (!empty($site_name)): ?>
         <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
       <?php endif; ?>
+      <a class="btn btn-default add-link navbar-brand" href="/p" type="button">
+        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> 发布
+      </a>
 
       <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
