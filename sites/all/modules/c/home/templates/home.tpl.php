bbdca93c45a390ec1c0800845407e81a16c518c8
Double H <h@obserbot.com>
2018-02-28 00:40:22 +0800

    dd

--- sites/all/modules/c/home/templates/home.tpl.php
+++ sites/all/modules/c/home/templates/home.tpl.php
@@ -1,6 +1,6 @@
   <div class="container">
     <div class="row">
-      <div class="col-sm-8">
+      <div class="col-sm-8 col-sm-offset-2 all-items">
         <div class="items-container text-center">
           <a href="/c/all">
             全部文章(<?php print $number_ding_yue_hao; ?>)
