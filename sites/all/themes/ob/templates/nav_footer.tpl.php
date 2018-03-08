ce8c78536e4990309b07cb77b1ae09ba230215ac
Double H <h@obserbot.com>
2018-03-08 12:06:56 +0800

    dd

--- sites/all/themes/ob/templates/nav_footer.tpl.php
+++ sites/all/themes/ob/templates/nav_footer.tpl.php
@@ -2,27 +2,25 @@
 <nav class="navbar navbar-default navbar-fixed-bottom visible-xs-block navfooter">
   <div class="container" style="margin-top:8px; padding-left:30px;">
     <div class="row">
-      <div class="text-center">
-        <div class="nav-item">
-          <a href="/">
-            <div class="item-wrap img-wrap">
-              <img src="/sites/all/themes/ob/images/home2.png" alt="home">
-            </div>
-            <div class="item-wrap text-wrap">
-              <span class="navfooter-text">主页</span>
-            </div>
-          </a>
-        </div>
-        <div>
-          <a href="/p">
-            <div class="item-wrap img-wrap">
-              <img src="/sites/all/themes/ob/images/add2.png" alt="post">
-            </div>
-            <div class="item-wrap text-wrap">
-              <span class="navfooter-text">发布</span>
-            </div>
-          </a>
-        </div>
+      <div class="nav-item">
+        <a href="/">
+          <div class="item-wrap img-wrap">
+            <img src="/sites/all/themes/ob/images/home2.png" alt="home">
+          </div>
+          <div class="item-wrap text-wrap">
+            <span class="navfooter-text">主页</span>
+          </div>
+        </a>
+      </div>
+      <div class="nav-item">
+        <a href="/p">
+          <div class="item-wrap img-wrap">
+            <img src="/sites/all/themes/ob/images/add2.png" alt="post">
+          </div>
+          <div class="item-wrap text-wrap">
+            <span class="navfooter-text">发布</span>
+          </div>
+        </a>
       </div>
     </div>
   </div>
