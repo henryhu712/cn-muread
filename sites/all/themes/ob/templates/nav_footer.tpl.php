6ea45b544fe71da7ab8505beb8ab90d4d3874627
Double H <h@obserbot.com>
2018-03-08 11:34:12 +0800

    dd

--- sites/all/themes/ob/templates/nav_footer.tpl.php
+++ sites/all/themes/ob/templates/nav_footer.tpl.php
@@ -1,18 +1,18 @@
 
-<nav class="navbar navbar-default navbar-fixed-bottom visible-xs-block">
+<nav class="navbar navbar-default navbar-fixed-bottom visible-xs-block navfooter">
   <div class="container" style="margin-top:8px; padding-left:30px;">
     <div class="row">
       <div class="text-center">
         <span style="margin-right:30px;">
           <a href="/">
             <img src="/sites/all/themes/ob/images/home.png" alt="home">
-            主页
+            <span class="navfooter-text"> 主页</span>
           </a>
         </span>
         <span>
           <a href="/p">
             <img src="/sites/all/themes/ob/images/add.png" alt="post">
-            发布
+            <span class="navfooter-text"> 发布</span>
           </a>
         </span>
       </div>
