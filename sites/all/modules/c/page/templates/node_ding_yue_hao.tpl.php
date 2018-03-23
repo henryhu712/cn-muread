e177a5f54129f44413a01979d0fdf8699199af16
Double H <h@obserbot.com>
2018-03-23 10:13:40 +0800

    dd

--- sites/all/modules/c/page/templates/node_ding_yue_hao.tpl.php
+++ sites/all/modules/c/page/templates/node_ding_yue_hao.tpl.php
@@ -1,22 +1,27 @@
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <header>
-    <span class="submitted">
-      <?php print $published; ?>
-    </span>
-    <span class="submitted">
-      <?php print $author; ?>
-    </span>
-  </header>
-  <div class="article-cover">
-    <img src="<?php print $image_url; ?>" alt="cover">
-  </div>
-  <div class="article-body">
-    <?php //print render($content['body']); ?>
-    <?php print $body_html; ?>
-  </div>
-  <div class="content-image">
-    <img src="<?php print $content_image; ?>" alt="content">
-  </div>
+
+  <?php if ($contentImageExists): ?>
+    <div class="content-image">
+      <?php print $content_image; ?>
+    </div>
+  <?php else: ?>
+    <header>
+      <span class="submitted">
+        <?php print $published; ?>
+      </span>
+      <span class="submitted">
+        <?php print $author; ?>
+      </span>
+    </header>
+    <div class="article-cover">
+      <img src="<?php print $image_url; ?>" alt="cover">
+    </div>
+    <div class="article-body">
+      <?php //print render($content['body']); ?>
+      <?php print $body_html; ?>
+    </div>
+  <?php endif; ?>
+
   <div class="article-origin">
     <a href="<?php print $url_origin; ?>" type="button" class="btn btn-success">阅读原文</a>
   </div>
