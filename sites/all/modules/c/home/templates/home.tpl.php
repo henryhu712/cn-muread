3655016083cca321c0b10ab7f1bb2c785dbf8c79
Double H <h@obserbot.com>
2018-03-26 20:07:10 +0800

    dd

--- sites/all/modules/c/home/templates/home.tpl.php
+++ sites/all/modules/c/home/templates/home.tpl.php
@@ -1,4 +1,38 @@
   <div class="container">
+
+    <div class="row home-content">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+        <div class="main-content-wrapper">
+          <?php foreach ($newest_articles as $key => $item): ?>
+            <div class="row news-item-wrap<?php print $key % 2 === 0 ? " news-odd-row" : ""; ?>">
+              <div class="col-xs-5">
+                <a href="<?php print $item['url_node']; ?>" target="_blank">
+                  <?php print $item['image']; ?>
+                </a>
+              </div>
+              <div class="col-xs-7 item-detail">
+                <div class="news-title">
+                  <a href="<?php print $item['url_node']; ?>" target="_blank">
+                    <?php print $item['title']; ?>
+                  </a>
+                </div>
+
+                <?php if (!empty($item['desc'])): ?>
+                <div class="news-desc hidden">
+                  <span class="desc"><?php print $item['desc']; ?></span>
+                </div>
+                <?php endif; ?>
+
+                <div class="news-footer">
+                  <?php print $item['created']; ?>
+                </div>
+              </div>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </div>
+
     <div class="row">
       <div class="col-sm-8 col-sm-offset-2 all-items">
         <div class="items-container text-center">
