a2b4d2dd2965a7cf76f956871071db061a8de2cb
Double H <h@obserbot.com>
2018-05-31 13:33:58 +0800

    template

---
+++ sites/all/modules/c/page/templates/ttee.tpl.php
@@ -0,0 +1,10 @@
+<p>kk</p>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <?php //print $webform_paste_url; ?>
+        </div>
+      </div>
+    </div>
+
+
